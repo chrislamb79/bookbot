407947b72badf8b414ae739b15f6a32bae884186
Chris Lamb <chris.lamb@autotrader.co.uk>
2026-03-08 17:38:01 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        words = file_contents.split( )
+        num_words = len(words)
+        print(f"Found {num_words} total words")
+
+def main():
+    get_book_text("books/frankenstein.txt")
+
+main()
+
+
+
+
+    
