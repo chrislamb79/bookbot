7a94e73401c2ba63cc9fc58ac2a6a9e4fe1113b8
Chris Lamb <chris.lamb@autotrader.co.uk>
2026-03-09 11:49:58 +0000

    ch2 l6

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from stats import get_book_text
+from stats import get_book_text, char_count
 
 def main():
-    get_book_text("books/frankenstein.txt")
-
+    # get_book_text("books/frankenstein.txt")
+    char_count("books/frankenstein.txt")
+    
 main()
 
 
