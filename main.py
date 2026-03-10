7c06d4907f3b8579c579fc44cddf5ffe27cf71fc
Chris Lamb <chris.lamb@autotrader.co.uk>
2026-03-10 15:10:43 +0000

    CH2 L3

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_test(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    print(file_contents)   
+    
+def main():
+    get_book_test("books/frankenstein.txt")
+
+main()
