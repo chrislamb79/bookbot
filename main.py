5291bf539d607b10610b66c9289b69e03dd279a2
Chris Lamb <chris.lamb@autotrader.co.uk>
2026-03-10 16:06:35 +0000

    CH2 L6

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_num_words
+from stats import count_num_words, get_chars_dict
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,6 +9,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = count_num_words(text)
+    chars_dict = get_chars_dict(text)
     print(f"Found {num_words} total words")
+    print(chars_dict)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def count_num_words(text):
     words = text.split()
     return len(words)
+
+def get_chars_dict(text):
+    chars = {}
+    for t in text:
+        lowered = t.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
